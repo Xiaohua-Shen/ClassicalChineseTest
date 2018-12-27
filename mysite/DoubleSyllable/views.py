2b68d811b8c3285f4c3b558073d75240109079fe
Ubuntu <ubuntu@ip-172-31-20-61.ap-southeast-1.compute.internal>
2018-12-27 10:05:56 +0000

    add preview function

--- mysite/DoubleSyllable/views.py
+++ mysite/DoubleSyllable/views.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import DoubleSyllable
+from .models import DoubleSyllableAccess
+from django.http import HttpResponse
+from django.template import loader
+from django.utils import timezone
 
 # Create your views here.
+def preview(request):
+    current_user = request.user
+    total_words = DoubleSyllable.objects.count()
+    
+    # already acessed word list
+    accessed_word_list = DoubleSyllableAccess.objects.filter(user_id__exact=current_user.id).values('doublesyllable_id').distinct()
+    accessed_words = accessed_word_list.count()
+
+    # fetch one unaccessed word 
+    word = DoubleSyllable.objects.exclude(id__in=accessed_word_list).values('word')[0]
+    # fetch work list by word
+    word_list = DoubleSyllable.objects.filter(word__exact=word['word'])
+    template = loader.get_template('DoubleSyllable/preview.html')
+    context = {
+        'word_list': word_list,
+        'total_words': total_words,
+        'accessed_words': accessed_words + 1,
+    }
+    # mark this word as accessed
+    for word in word_list:
+        DoubleSyllableAccess.objects.create(user_id=current_user.id,doublesyllable_id=word.id,access_date=timezone.now()) 
+    # return response
+    return HttpResponse(template.render(context, request))
