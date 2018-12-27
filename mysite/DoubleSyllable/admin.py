d6ae6fb3e0754244c274f39fd510daaaaf95efda
Ubuntu <ubuntu@ip-172-31-20-61.ap-southeast-1.compute.internal>
2018-12-27 02:53:30 +0000

    initial function

---
+++ mysite/DoubleSyllable/admin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from django.forms import TextInput, Textarea
+from django.db import models
+
+# Register your models here.
+from .models import DoubleSyllable
+
+class DoubleSyllableAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'80'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
+    fieldsets = [
+        ('双音词',               {'fields': ['word']}),
+        ('古今异义', {'fields': ['classical', 'current']}),
+        ('例句', {'fields': ['source', 'sample', 'translation']}),
+    ]
+    list_display = ('word', 'classical','current','sample','source')
+    search_fields = ['word']
+
+admin.site.register(DoubleSyllable, DoubleSyllableAdmin)
---
+++ mysite/DoubleSyllable/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models  here.
+@python_2_unicode_compatible
+class DoubleSyllable(models.Model):
+    word = models.CharField(max_length=10)
+    classical = models.CharField(max_length=100)
+    current = models.CharField(max_length=100)
+    sample = models.CharField(max_length=120)
+    translation = models.CharField(max_length=200)
+    source = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.word
+
+class DoubleSyllableAccess(models.Model):
+    doublesyllable = models.ForeignKey(DoubleSyllable, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    access_date = models.DateTimeField('date accessed')
+
+class DoubleSyllableTest(models.Model):
+    doublesyllable = models.ForeignKey(DoubleSyllable, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    test_date = models.DateTimeField('test accessed')
+    test_result = models.IntegerField(default=0)
