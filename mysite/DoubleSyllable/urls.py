085daf9cdba0322c9d9c80081389c32077e34586
Xiaohua-Shen <shenxh@cn.ibm.com>
2018-12-30 14:31:13 +0800

    submit test result to server

--- mysite/DoubleSyllable/urls.py
+++ mysite/DoubleSyllable/urls.py
@@ -6,5 +6,5 @@ app_name = 'DoubleSyllable'
 urlpatterns = [
     url(r'^preview/$', views.preview, name='preview'),
     url(r'^(?P<doublesyllable_id>[0-9]+)/test/$', views.test, name='test'),
-    url(r'^(?P<doublesyllable_id>[0-9]+)/result/$', views.test, name='result'),
+    url(r'^(?P<doublesyllable_id>[0-9]+)/result/$', views.result, name='result'),
 ]
