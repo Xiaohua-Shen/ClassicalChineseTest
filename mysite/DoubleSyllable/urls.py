9a331fa9ebcfb1aae122ffebed515117ac86df91
Xiaohua-Shen <shenxh@cn.ibm.com>
2018-12-30 16:59:42 +0800

    fix typo

--- mysite/DoubleSyllable/urls.py
+++ mysite/DoubleSyllable/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'DoubleSyllable'
 urlpatterns = [
-    url(r'^/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^preview/$', views.preview, name='preview'),
     url(r'^(?P<doublesyllable_id>[0-9]+)/result/$', views.result, name='result'),
 ]
